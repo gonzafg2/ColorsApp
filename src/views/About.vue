c373ef06253064112acaf7a5287ce658a9549768
Gonzalo Fleming <gonzafg2@gmail.com>
2020-05-14 17:16:42 -0400

    Corrijo errores de vista about

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,18 +7,16 @@
     <section class="colors-tag-copy__copied">
       <div class="colors-tag--name-code">
         Esta aplicación fue realizada tomando los datos de la paleta de colores
-        obtenidos de esta API
-        <a href="https://reqres.in/api/colors">https://reqres.in/api/colors</a>
-        generada por nuestra empresa. Esto con el objetivo de facilitar la
-        visualización y copiado de los códigos hexadecimales de colores dentro
-        de la paleta institucional.
+        obtenidos desde esta API
+        <a href="https://reqres.in/api/colors">https://reqres.in/api/colors</a>. Esto con el objetivo de facilitar la
+        visualización y copiado de los códigos hexadecimales de colores requeridos.
       </div>
     </section>
 
     <section class="colors-page">
-      <router-link to="/Page1">&#8592; Página 1</router-link>
-      <div class="colors-page--site">Sobre esta aplicación</div>
-      <router-link to="/Page2">Página 2 &#8594;</router-link>
+      <router-link :to="{ name: 'MainP1' }">>&#8592; Página 1</router-link>
+      <div class="colors-page--site">Sobre esta app</div>
+      <router-link :to="{ name: 'MainP2' }">Página 2 &#8594;</router-link>
     </section>
   </main>
 </template>
