7fb90997746ebe7681295aeb6acceb60062240a8
Gonzalo Fleming <gonzafg2@gmail.com>
2020-05-14 14:56:28 -0400

    Corrijo detalles para vista movil

--- src/views/About.vue
+++ src/views/About.vue
@@ -34,4 +34,27 @@
     text-decoration: underline;
   }
 }
+@media screen and (max-width: 767px){
+  .colors-tag-copy__copied {
+    color: #000;
+    box-shadow: -2px 4px 1px #fff;
+    .colors-tag--name-code {
+      font-size: 1rem;
+    }
+    & a {
+      text-decoration: underline;
+    }
+  }
+  .colors-page {
+    grid-column: 1/2;
+    font-size: .75rem;
+    line-height: 1.8;
+  }
+  .colors-page--site {
+    font-size: .8rem;
+  }
+  .nav-items a.link-about{
+    font-size: .7rem;
+  }
+}
 </style>
